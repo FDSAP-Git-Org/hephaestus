08a108fecd0ec61befe460afeb767ece686a0e74
Raymond Bago <raymond.bago@fortress-asya.com>
2024-08-19 10:06:13 +0800

    added logger for api

--- apilogs/apilogs.go
+++ apilogs/apilogs.go
@@ -58,9 +58,35 @@ func SystemLog(class, folder, filename, process string, request, response interf
 
 	Separator.Println("")
 	InfoLogger.Println(class + ": - - - - : " + strings.ToUpper(process) + " : - - - -")
-	InfoLogger.Println(class + ": PROCESS TIME	: " + currentTime.Format(time.DateTime))
-	InfoLogger.Println(class + ": REQUEST		: " + string(strRequest))
-	InfoLogger.Println(class + ": RESPONSE		: " + string(strResponse))
+	InfoLogger.Println(class + ": PROCESS TIME: " + currentTime.Format(time.DateTime))
+	InfoLogger.Println(class + ": REQUEST: " + string(strRequest))
+	InfoLogger.Println(class + ": RESPONSE: " + string(strResponse))
 
-	fmt.Println("New system log entry:", currentTime.Format(time.DateTime))
+	fmt.Printf("New entry for %s: %v\n", strings.ToUpper(process), currentTime.Format(time.DateTime))
+}
+
+func APIlog(class, folder, filename, process, status string, request, response interface{}) {
+	// Checking folder name if exists
+	currentTime := time.Now()
+	folderName := "./logs/" + strings.ToUpper(folder) + "/" + currentTime.Format("01-January")
+	CreateDirectory(folderName)
+	file, filErr := os.OpenFile(folderName+"/"+strings.ToLower(filename)+"-"+currentTime.Format("01022006")+".log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if filErr != nil {
+		fmt.Println("error wrting the logs:", filErr.Error())
+	}
+
+	strRequest, _ := json.Marshal(request)
+	strResponse, _ := json.Marshal(response)
+
+	InfoLogger = log.New(file, "INFO: ", log.Ldate|log.Ltime)
+	Separator = log.New(file, "", log.Ldate|log.Ltime)
+
+	Separator.Println("")
+	InfoLogger.Println(class + ": - - - - : " + strings.ToUpper(process) + " : - - - -")
+	InfoLogger.Println(class + ": PROCESS TIME: " + currentTime.Format(time.DateTime))
+	InfoLogger.Println(class + ": REQUEST: " + string(strRequest))
+	InfoLogger.Println(class + ": RESPONSE: " + string(strResponse))
+	InfoLogger.Println(class + ": STATUS: " + status)
+
+	fmt.Printf("New entry for %s: %v\n", strings.ToUpper(process), currentTime.Format(time.DateTime))
 }
