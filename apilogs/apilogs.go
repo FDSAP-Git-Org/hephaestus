77cc9f11ca67bf612d7335269653ab7dfabda96b
Raymond Bago <raymond.bago@fortress-asya.com>
2024-08-30 14:15:12 +0800

    fixed error logs, wrong infologger

--- apilogs/apilogs.go
+++ apilogs/apilogs.go
@@ -65,33 +65,7 @@ func SystemLog(class, folder, filename, process string, request, response interf
 	fmt.Printf("New entry for %s: %v\n", strings.ToUpper(process), currentTime.Format(time.DateTime))
 }
 
-func APIlog(class, folder, filename, process, code string, request, err interface{}) {
-	// Checking folder name if exists
-	currentTime := time.Now()
-	folderName := "./logs/" + strings.ToUpper(folder) + "/" + currentTime.Format("01-January")
-	CreateDirectory(folderName)
-	file, filErr := os.OpenFile(folderName+"/"+strings.ToLower(filename)+"-"+currentTime.Format("01022006")+".log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
-	if filErr != nil {
-		fmt.Println("error wrting the logs:", filErr.Error())
-	}
-
-	strRequest, _ := json.Marshal(request)
-	strResponse, _ := json.Marshal(err)
-
-	ErrorLogger = log.New(file, "ERROR: ", log.Ldate|log.Ltime)
-	Separator = log.New(file, "", log.Ldate|log.Ltime)
-
-	Separator.Println("")
-	ErrorLogger.Println(class + ": - - - - : " + strings.ToUpper(process) + " : - - - -")
-	ErrorLogger.Println(class + ": PROCESS TIME: " + currentTime.Format(time.DateTime))
-	ErrorLogger.Println(class + ": REQUEST: " + string(strRequest))
-	ErrorLogger.Println(class + ": CODE: " + string(strResponse))
-	ErrorLogger.Println(class + ": ERROR: " + string(strResponse))
-
-	fmt.Printf("New entry for %s: %v\n", strings.ToUpper(process), currentTime.Format(time.DateTime))
-}
-
-func APIErrorlog(class, folder, filename, process, status string, request, response interface{}) {
+func APIlog(class, folder, filename, process, status string, request, response interface{}) {
 	// Checking folder name if exists
 	currentTime := time.Now()
 	folderName := "./logs/" + strings.ToUpper(folder) + "/" + currentTime.Format("01-January")
@@ -104,7 +78,7 @@ func APIErrorlog(class, folder, filename, process, status string, request, respo
 	strRequest, _ := json.Marshal(request)
 	strResponse, _ := json.Marshal(response)
 
-	InfoLogger = log.New(file, "INFO: ", log.Ldate|log.Ltime)
+	InfoLogger = log.New(file, "ERROR: ", log.Ldate|log.Ltime)
 	Separator = log.New(file, "", log.Ldate|log.Ltime)
 
 	Separator.Println("")
@@ -113,6 +87,32 @@ func APIErrorlog(class, folder, filename, process, status string, request, respo
 	InfoLogger.Println(class + ": REQUEST: " + string(strRequest))
 	InfoLogger.Println(class + ": RESPONSE: " + string(strResponse))
 	InfoLogger.Println(class + ": STATUS: " + status)
+
+	fmt.Printf("New entry for %s: %v\n", strings.ToUpper(process), currentTime.Format(time.DateTime))
+}
+
+func APIlErrorog(class, folder, filename, process, code string, request, err interface{}) {
+	// Checking folder name if exists
+	currentTime := time.Now()
+	folderName := "./logs/" + strings.ToUpper(folder) + "/" + currentTime.Format("01-January")
+	CreateDirectory(folderName)
+	file, filErr := os.OpenFile(folderName+"/"+strings.ToLower(filename)+"-"+currentTime.Format("01022006")+".log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if filErr != nil {
+		fmt.Println("error wrting the logs:", filErr.Error())
+	}
+
+	strRequest, _ := json.Marshal(request)
+	strResponse, _ := json.Marshal(err)
+
+	ErrorLogger = log.New(file, "INFO: ", log.Ldate|log.Ltime)
+	Separator = log.New(file, "", log.Ldate|log.Ltime)
+
+	Separator.Println("")
+	ErrorLogger.Println(class + ": - - - - : " + strings.ToUpper(process) + " : - - - -")
+	ErrorLogger.Println(class + ": PROCESS TIME: " + currentTime.Format(time.DateTime))
+	ErrorLogger.Println(class + ": REQUEST: " + string(strRequest))
+	ErrorLogger.Println(class + ": CODE: " + code)
+	ErrorLogger.Println(class + ": ERROR: " + string(strResponse))
 
 	fmt.Printf("New entry for %s: %v\n", strings.ToUpper(process), currentTime.Format(time.DateTime))
 }
