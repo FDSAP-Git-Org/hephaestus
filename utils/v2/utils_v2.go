aefc71f985023a913e33b74d80613cd7b40dc828
Raymond Bago <raymond.bago@fortress-asya.com>
2025-02-27 12:35:24 +0800

    added request with status

--- utils/v2/utils_v2.go
+++ utils/v2/utils_v2.go
@@ -76,6 +76,69 @@ func SendRequest(baseURL string, method string, body []byte, headers map[string]
 	return nil, fmt.Errorf("response is neither a JSON object nor a JSON array: %s", string(body))
 }
 
+func SendRequesWithStatus(baseURL string, method string, body []byte, headers map[string]string, queryParam map[string]interface{}, timeout int) (interface{}, *string, error) {
+	reqBody := bytes.NewBuffer(body)
+
+	finalUrl := baseURL
+
+	for qkey, qvalue := range queryParam {
+		finalUrl = finalUrl + "?" + qkey + "=" + fmt.Sprint(qvalue)
+	}
+
+	fmt.Println("Final Url", finalUrl)
+	// Create the request
+	req, err := http.NewRequest(method, finalUrl, reqBody)
+	if err != nil {
+		return nil, &req.Response.Status, err
+	}
+
+	// Set default content-type header if not provided
+	if _, exists := headers["Content-Type"]; !exists {
+		req.Header.Set("Content-Type", "application/json")
+	}
+
+	// Add custom headers
+	for key, value := range headers {
+		req.Header.Set(key, value)
+	}
+
+	client := &http.Client{Timeout: time.Second * time.Duration(timeout)}
+
+	// Send the request
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, &resp.Status, err
+	}
+
+	defer resp.Body.Close()
+
+	// Read response body
+	body, err = io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, &resp.Status, err
+	}
+
+	// Handle empty response
+	if len(body) == 0 {
+		return nil, nil, nil
+	}
+
+	// Try to parse response as JSON object
+	var jsonRespObject map[string]interface{}
+	if err := json.Unmarshal(body, &jsonRespObject); err == nil {
+		return jsonRespObject, &resp.Status, nil
+	}
+
+	// If parsing as JSON object fails, try as JSON array
+	var jsonRespArray []interface{}
+	if err := json.Unmarshal(body, &jsonRespArray); err == nil {
+		return jsonRespArray, &resp.Status, nil
+	}
+
+	// If neither parsing works, return an error
+	return nil, nil, fmt.Errorf("response is neither a JSON object nor a JSON array: %s", string(body))
+}
+
 func GenerateUIID(appName string) string {
 	uiid := uuid.New()
 	return fmt.Sprintf("%s-%s", appName, uiid.String())
